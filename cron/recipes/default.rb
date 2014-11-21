6c55f61af97aefd56b07c19dfd700cbf60b91d6b
Shubhajit Saha <suvozit@live.com>
2014-11-21 10:47:30 +0530

    new cron job

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -1,4 +1,23 @@
-cron "sync_service oauth task" do
-  minute "*/2"
-  command "cd /srv/www/log_pixel/current && php index.php task sync_service oauth"
+# cron "sync_service oauth task" do
+#   minute "*/2"
+#   command "cd /srv/www/log_pixel/current && php index.php task sync_service oauth"
+# end
+
+# 
+# Author:: Juan Manuel Torres <kinojman@gmail.com>
+# Cookbook Name:: cronjobs
+# Recipe:: default
+#
+# Create custom cron jobs using configuration values in the Custom JSON
+#
+
+node[:custom_env][:cron_jobs].each do |cron_values|
+  cron "#{cron_values[:name]}" do
+    minute  "#{cron_values[:minute]}"
+    hour    "#{cron_values[:hour]}"
+    day     "#{cron_values[:day]}"
+    month   "#{cron_values[:month]}"
+    weekday "#{cron_values[:weekday]}"
+    command "#{cron_values[:command]}"
+  end
 end
