77cf2a2bd9435e032cfb80f13af757aef621e818
Shubhajit Saha <suvozit@live.com>
2015-02-25 02:25:04 +0530

    single cron

--- cronjobs2/recipes/default.rb
+++ cronjobs2/recipes/default.rb
@@ -1,17 +1,4 @@
-# 
-# Author:: Juan Manuel Torres <kinojman@gmail.com>
-# Cookbook Name:: cronjobs
-# Recipe:: default
-#
-# Create custom cron jobs using configuration values in the Custom JSON
-#
-
-cron "echo hi" do
-  minute "*/1"
-  command "%Q{ echo 'hi' >> /home/ubuntu/test.txt }"
-end
-
-cron "echo hi" do
+cron "sync_url extract" do
   minute "*/1"
   command "cd /srv/www/log_pixel/current && php index.php task sync_url extract   60"
 end
