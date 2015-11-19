b09bc67e99d3a3dce191fdbd8fbbf1b886633c56
Shubhajit Saha <suvozit@live.com>
2015-11-19 23:51:38 +0530

    more /tmp directories

--- aws-cache/recipes/default.rb
+++ aws-cache/recipes/default.rb
@@ -4,3 +4,17 @@ directory "/tmp/aws-cache" do
   group 'root'
   action :create
 end
+
+directory "/tmp/ci" do
+  mode 0777
+  owner 'root'
+  group 'root'
+  action :create
+end
+
+directory "/tmp/ci/upload" do
+  mode 0777
+  owner 'root'
+  group 'root'
+  action :create
+end
